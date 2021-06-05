fbe2c0c12f8caa8b5bf12dc728253face3a43ae8
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2021-06-05 19:28:14 -0400

    Added a new auth listener hook.

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,23 +1,47 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 import { Home, Browse, Signin, Signup } from './pages';
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
+import { useAuthListener } from './hooks';
 
 const App = () => {
+  const { user } = useAuthListener();
+
   return (
     <Router>
-      <Route exact path={ROUTES.BROWSE}>
-        <Browse />
-      </Route>
-      <Route exact path={ROUTES.SIGN_UP}>
-        <Signup />
-      </Route>
-      <Route exact path={ROUTES.SIGN_IN}>
-        <Signin />
-      </Route>
-      <Route exact path={ROUTES.HOME}>
-        <Home />
-      </Route>
+      <Switch>
+        <IsUserRedirect 
+          user={user} 
+          loggedInPath={ROUTES.BROWSE} 
+          path={ROUTES.SIGN_IN}
+          exact
+        >
+          <Signin />
+        </IsUserRedirect>
+        <IsUserRedirect 
+          user={user} 
+          loggedInPath={ROUTES.BROWSE} 
+          path={ROUTES.SIGN_UP}
+          exact
+        >
+          <Signup />
+        </IsUserRedirect>
+        <ProtectedRoute
+          user={user}
+          path={ROUTES.BROWSE}
+          exact
+        >
+          <Browse />
+        </ProtectedRoute>
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.HOME}
+        >
+          <Home />
+        </IsUserRedirect>
+      </Switch>
     </Router>
   );
 }
